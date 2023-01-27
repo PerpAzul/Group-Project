95369c6f5ad8dc51d3d2fe2505a2acf9e447d4f5
23-Lukas <lukas.daniek@gmail.com>
2023-01-27 13:27:56 +0100

    Prefabs und Spawn Code für die PowerUps

--- Group Project/Assets/GameScripts/SpawnItems.cs
+++ Group Project/Assets/GameScripts/SpawnItems.cs
@@ -7,14 +7,19 @@ public class SpawnItems : MonoBehaviour
     
     public GameObject Ammo;
     public GameObject Healz;
+    public GameObject InvisPowerUp;
+    public GameObject DoubleDamagePowerUp;
     public Transform AmmoSpawn;
     public Transform HeartSpawn;
+    public Transform PowerUpSpawn;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTimerAmmoCoroutine());
         StartCoroutine(SpawnTimerHealzCoroutine());
+        StartCoroutine(SpawnTimerInvisCoroutine());
+        StartCoroutine(SpawnTimerDoubleDamageCoroutine());
     }
     
     IEnumerator SpawnTimerAmmoCoroutine()
@@ -31,6 +36,22 @@ public class SpawnItems : MonoBehaviour
         {
             yield return new WaitForSeconds(Random.Range(25f, 35f));
             Instantiate(Healz, HeartSpawn.position, HeartSpawn.rotation);
+        }
+    }
+    IEnumerator SpawnTimerInvisCoroutine()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(Random.Range(25f, 35f));
+            Instantiate(InvisPowerUp, PowerUpSpawn.position, PowerUpSpawn.rotation);
+        }
+    }
+    IEnumerator SpawnTimerDoubleDamageCoroutine()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(Random.Range(25f, 35f));
+            Instantiate(DoubleDamagePowerUp, PowerUpSpawn.position, PowerUpSpawn.rotation);
         }
     }
 }
